008f08fc7cfb8856d950f71da594a20e1ea21f6a
Valeriia Styfurak <valeriiastyfurak@gmail.com>
2023-12-15 11:29:43 +0200

    clear code

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -5,19 +5,12 @@
   const body = document.querySelector('body');
 
   openMenuBtnRef.addEventListener('click', () => {
-    // mobileMenuRef.classList.add('is-open');
     body.classList.add('overflow-hidden','is-open');
   });
 
   closeMenuBtnRef.addEventListener('click', () => {
-    // mobileMenuRef.classList.remove('is-open');
     body.classList.remove('overflow-hidden', 'is-open');
   });
 })();
 
-// const mobileMenuRef = document.querySelector('#burger-menu');
-//
-// mobileMenuRef.addEventListener('click', () => {
-//   mobileMenuRef.classList.toggle('is-open');
-// })
 
