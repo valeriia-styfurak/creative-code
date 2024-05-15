7a3e12589ce71d19dbf6e8a9d9b46c5e9dbf8151
Valeriia Styfurak <valeriiastyfurak@gmail.com>
2024-05-15 22:01:26 +0300

    delete variable

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,10 +1,8 @@
 (() => {
   const openMenuBtnRef = document.querySelector('[data-open-menu-button]');
   const closeMenuBtnRef = document.querySelector('[data-close-menu-button]');
-  const mobileMenuRef = document.querySelector('[data-mobile-menu]');
   const body = document.querySelector('body');
   const mobileLink = document.querySelector('.mobile-nav-link')
-  const mobileButton = document.querySelector('.mobile-nav-button')
 
   openMenuBtnRef.addEventListener('click', () => {
     body.classList.add('overflow-hidden','is-open');
@@ -18,4 +16,3 @@
     body.classList.remove('overflow-hidden', 'is-open');
   })
 })();
-
