7caa589a043dbde87805f08a52d21044294f3bfe
Valeriia Styfurak <valeriiastyfurak@gmail.com>
2024-05-16 15:01:11 +0300

    remove class

--- src/js/anchor-scroll.js
+++ src/js/anchor-scroll.js
@@ -2,6 +2,8 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
 
+    document.body.classList.remove('mobile-menu-open');
+
     document.querySelector(this.getAttribute('href')).scrollIntoView({
       behavior: 'smooth',
     });
